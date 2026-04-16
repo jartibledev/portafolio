97a818752447d2dfcde547d29847fbb074f74f46
Jartible <jartibledev@proton.me>
2026-04-16 21:54:57 +0200

    Rename the file StylesParagraph.js to StylesParagraps.styles.js and rewrite the file in styled components

---
+++ src/app/styles/StylesParagraph.styles.js
@@ -0,0 +1,159 @@
+import styled from 'styled-componets'
+ 
+const greyColor= " #686868"
+export const Head = styled.h1`
+    font-family: var(--font-dm-mono);
+    font-size: 5 rem ;
+    font-weight: normal;
+    color : black;
+    letter-sparcing: -5%;
+    line-height: 110%; 
+`;
+
+export const Subtitle = styled.h2`
+    font-family: var(--font-dm-mono);
+    font-weight: medium;
+    font-size: 0.9 rem ;
+    color : ${greyColor} ;
+
+`;
+
+export const Dropline = styled.h3`
+    font-family: var(--font-geist-mono);
+    font-weight: bold;
+    font-size: 0.9 rem ;
+    color : ${greyColor} ;
+
+`;
+
+export const Contact = styled.p`
+    font-family: var(--font-dm-mono);
+    font-weight: normal;
+    font-size: 1 rem ;
+    color : black ;
+     line-height: 145%; 
+
+`;
+export const Button = styled.p`
+    font-family: var(--font-cascadia-mono);
+    font-weight: 600 ;
+    font-size: 1 rem ;
+    color : black ;
+     line-height: 145%; 
+
+`;
+export const Footer = styled.p`
+    font-family: var(--font-dm-mono);
+    font-weight: medium ;
+    font-size: 2 rem ;
+    color : black ;
+    letter-spacing: -2%;
+     line-height: 145%; 
+
+`;
+
+export const FooterText = styled.p`
+    font-family: var(--font-dm-mono);
+    font-weight: medium ;
+    font-size: 1 rem ;
+    color : ${greyColor} ;
+    letter-spacing: -2%;
+     line-height: 145%; 
+
+`;
+export const FooterSubtitle = styled.p`
+    font-family: var(--font-dm-mono);
+    font-weight: medium ;
+    font-size: 1 rem ;
+    color : ${greyColor} ;
+    letter-spacing: -2%;
+     line-height: 145%; 
+
+`;
+export const Projects = styled.p`
+    font-family: var(--font-fragment-mono);
+    font-weight: regular ;
+    font-size: 2 rem ;
+    color : black ;
+    letter-spacing: -2%;
+     line-height: 120%; 
+
+`;
+
+export const Date = styled.p`
+    font-family: var(--font-fragment-mono);
+    font-weight: regular ;
+    font-size: 1.8 rem ;
+    color : ${greyColor} ;
+    letter-spacing: -2%;
+     line-height: 120%; 
+
+`;
+
+export const LabelSocialMedia = styled.p`
+    font-family: var(--font-fragment-mono);
+    font-weight: regular ;
+    font-size: 2 rem ;
+    color : black ;
+    letter-spacing: -2%;
+     line-height: 120%; 
+
+`;
+
+export const SocialNetwork = styled.p`
+    font-family: var(--font-fragment-mono);
+    font-weight: regular ;
+    font-size: 1.8 rem ;
+    color : black ;
+     line-height: 145%; 
+
+`;
+export const PortafolioPress = styled.p`
+    font-family: var(--font-cutive-mono);
+    font-weight: regular ;
+    font-size: 2.5 rem ;
+    color : black ;
+    letter-spacing: -2.5%;
+     line-height: 110%; 
+
+`;
+
+export const SectionPressed = styled.p`
+    font-family: var(--font-dm-mono);
+    font-weight: lighter ;
+    font-size: 3 rem ;
+    color : black ;
+    letter-spacing: -2.5%;
+     line-height: 110%; 
+
+`;
+
+export const Portafolio = styled.p`
+    font-family: var(--font-inter);
+    font-weight: bold ;
+    font-size: 2 rem ;
+    color : black ;
+    letter-spacing: -2.5%;
+     line-height: 110%; 
+
+`;
+
+export const Work = styled.p`
+    font-family: var(--font-inter);
+    font-weight: bold ;
+    font-size: 3 rem ;
+    color : black ;
+    letter-spacing: -2%;
+     line-height: 110%; 
+
+`;
+
+
+
+
+
+
+ 
+
+
+
