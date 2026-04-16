08d6c4321816f65fb08efe86aa04929441692368
Jartible <jartibledev@proton.me>
2026-04-16 20:38:46 +0200

    Write some paragraph styles

---
+++ src/app/styles/stylesParagraph.js
@@ -0,0 +1,34 @@
+export const head = {
+    fontFamily : "DM Mono",
+    fontType : "regular",
+    fontSize : "5 rem",
+    color : "black"
+     
+};
+
+export const subtitle = {
+    fontFamily : "DM Mono",
+    fontType : "regular",
+    fontSize : "0,9 rem",
+    color : "#686868"
+     
+};
+
+export const dropline = {
+    fontFamily : "Geist Mono",
+    fontType : "Bold",
+    fontSize : "0,9 rem",
+    color : "black"
+     
+};
+
+export const contact = {
+    fontFamily : "DM Mono",
+    fontType : "Regular",
+    fontSize : "1 rem",
+    color : "black"
+     
+};
+
+
+
