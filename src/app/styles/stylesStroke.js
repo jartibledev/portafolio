f7733bff7d5f4e19f26a3b5dd79255bc36d26a41
Jartible <jartibledev@proton.me>
2026-04-17 13:57:53 +0200

    Write the styles stroke in stylesStroke.js

---
+++ src/app/styles/stylesStroke.js
@@ -0,0 +1,48 @@
+import styled from 'styled-componets'
+
+const borderBox = "1.2 rem solid black ";
+const borderRadius = " border-radius: 0px";
+
+
+export const App = styled.div`
+    margin: 0;
+    padding: 0;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+`;
+
+export const ButtonStar = styled.div`
+    margin: 2 rem;
+    button {
+        border: ${borderBox};
+        cursor:pointer;  
+    }    
+`;
+
+
+export const ButtonPortafolio = styled.button`
+        border: ${borderBox};
+        border-radius: ${borderRadius};
+        cursor:pointer;
+        filter:blur(9px);
+        backdrop-filter: blur(9px); 
+        &:hover {
+            filter: none;
+            backdrop-filter: none;
+        } 
+        
+`;
+
+export const HeadPortafolio = styled.div`
+        border: ${borderBox};
+        border-radius: ${borderRadius};
+        cursor:pointer;
+        filter:blur(9px);
+        backdrop-filter: blur(9px); 
+        &:hover {
+            filter: none;
+            backdrop-filter: none;
+        } 
+        
+`;
