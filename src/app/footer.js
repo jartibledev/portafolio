985a38eccfeb7a33f964adb0761a94f8b8ae5dc9
Jartible <jartibledev@proton.me>
2026-04-20 13:28:03 +0200

    Write the template of files hero.js and footer.js

---
+++ src/app/footer.js
@@ -0,0 +1,8 @@
+function Footer(){
+    return(
+        <footer>
+            <p>Author: Hege Refsnes</p>
+            <p><a href="mailto:hege@example.com">hege@example.com</a></p>
+        </footer>
+    )
+}
